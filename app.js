31463eac64e4dc91ac25945d204b91e521eae82c
LubchikEgor <97447426+SpichkaXD@users.noreply.github.com>
2022-09-22 19:30:41 +0300

    project pr14 testFix2

--- app.js
+++ app.js
@@ -61,6 +61,14 @@ app.use('*', (req, res, next) => {
 
 app.use(errors());
 
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+  });
+  next();
+});
+
 async function main() {
   await mongoose.connect('mongodb://localhost:27017/mestodb', {
     useNewUrlParser: true,
